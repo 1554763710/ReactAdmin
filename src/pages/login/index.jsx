6e507fc3f0cde1e64f76276a7a45a84a60b5210a
Guo <1554763710@qq.com>
2019-04-01 14:55:11 +0800

    login静态页面

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,40 @@
+
+import React,{Component} from "react";
+import {
+  Form, Icon, Input, Button
+} from 'antd';
+
+import logo from "./logo.png";
+import "./index.less";
+
+export default class Login extends Component{
+  
+  handleSubmit = (e)=>{
+    e.preventDefault();
+}
+  
+  render(){
+    return <div className="login">
+      <header className="login-header">
+        <img src={logo} alt="logo"/>
+        <h1>React项目-后台管理系统</h1>
+      </header>
+      <section className="login-content">
+        <h2>用户管理</h2>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          <Form.Item>
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />
+          </Form.Item>
+          <Form.Item>
+              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </section>
+    </div>
+  }
+}
